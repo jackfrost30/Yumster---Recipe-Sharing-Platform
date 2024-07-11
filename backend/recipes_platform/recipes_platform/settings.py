d549d5057d314e1be14d3bc677972431b295346b
Praveen <kmhanuman8@gmail.com>
2024-07-10 20:38:32 -0500

    added postgres database

--- backend/recipes_platform/recipes_platform/settings.py
+++ backend/recipes_platform/recipes_platform/settings.py
@@ -100,12 +100,39 @@ WSGI_APPLICATION = "recipes_platform.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+'''
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+'''
+
+# Amazon RDS PostgreSQL database
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'Yumster',
+
+        'USER': 'praveen',
+
+        'PASSWORD': 'masterpraveen',
+
+        'HOST': 'yumsteraws.cja4iommkv1z.us-east-2.rds.amazonaws.com',
+
+        'PORT': '5432',
+    }
+}
+
+
+
 
 
 # Password validation
